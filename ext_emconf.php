86ca450a44644072218e060591ed729a82348010
Philipp Kitzberger <coding@kitze.net>
2022-08-01 13:26:24 +0200

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'ACLs from files',
+    'description' => '',
+    'category' => 'misc',
+    'version' => '0.0.1',
+    'state' => 'stable',
+    'clearCacheOnLoad' => true,
+    'author' => 'Philipp Kitzberger',
+    'author_email' => 'info@cron.eu',
+    'author_company' => 'cron IT GmbH',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
